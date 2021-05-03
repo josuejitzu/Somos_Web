c4bddee029c605d755097e0e104909f13fb8de46
josueJitzu <iosue27@gmail.com>
2021-05-03 17:48:32 -0500

    incrustado facebook

--- js/auditorio.js
+++ js/auditorio.js
@@ -192,3 +192,31 @@ botonMuteVideo.addEventListener('click',()=>{
     
 
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+	const source = 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
+	// const video = document.querySelector('video');
+	
+	// For more options see: https://github.com/sampotts/plyr/#options
+	// captions.update is required for captions to work with hls.js
+	// const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
+	
+	if (!Hls.isSupported()) {
+		videoPlayer.src = source;
+	} else {
+		// For more Hls.js options, see https://github.com/dailymotion/hls.js
+		const hls = new Hls();
+		hls.loadSource(source);
+		hls.attachMedia(videoPlayer);
+		window.hls = hls;
+		
+		// Handle changing captions
+		videoPlayer.on('languagechange', () => {
+			// Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
+			setTimeout(() => hls.subtitleTrack = videoPlayer.currentTrack, 50);
+		});
+	}
+	
+	// Expose player so it can be used from the console
+	window.player = videoPlayer;
+});
